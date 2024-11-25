8fe555100c3d14303659824ca22135918502e29e
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2024-11-25 17:27:54 +0000

    setup for explain flux

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,9 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -23,8 +20,6 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
@@ -34,4 +29,4 @@ const Layout = () => {
 	);
 };
 
-export default injectContext(Layout);
+export default Layout;
